87e318ea2f247aed07377de031e4d572439ba5f2
David Wagner <david.wagner@epfl.ch>
2011-10-25 19:15:25 +0200

    Quickly convert the output of the function.

--- xxdata_11.py
+++ xxdata_11.py
@@ -22,6 +22,21 @@ def read_scd(filename):
     os.unlink(fortran_filename)
     return ret
 
+
+def convert_to_dictionary(out):
+    iz0, is1min, is1max, nptnl, nptn, nptnc, iptnla, iptna, iptnca, ncnct,\
+    icnctv, iblmx, ismax, dnr_ele, dnr_ams, isppr, ispbr, isstgr, idmax,\
+    itmax, ddens, dtev, drcof, lres, lstan, lptn = out
+
+    d = {}
+    d['charge'] = iz0
+    d['density'] = ddens[:idmax]
+    d['temperature'] = dtev[:itmax]
+    d['number_of_charge_states'] = ismax
+    d['coeff_table'] = drcof[:ismax, :itmax, :idmax]
+    return d
+
+
 if __name__ == '__main__':
     out = read_scd('scd96_c.dat')
-    print out[0]
+    d = convert_to_dictionary(out)
