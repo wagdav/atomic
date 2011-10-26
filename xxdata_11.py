1cf2054a93115caa50a4a63569608dc818b9156d
David Wagner <david.wagner@epfl.ch>
2011-10-26 12:44:44 +0200

    Implement a reader for recombination files.

--- xxdata_11.py
+++ xxdata_11.py
@@ -1,6 +1,11 @@
 import os
 import _xxdata_11
 
+adf11_classes = {
+    'acd' : 1,
+    'scd' : 2,
+}
+
 parameters = {
     'isdimd' : 200,
     'iddimd' : 40,
@@ -11,16 +16,29 @@ parameters = {
     'ndcnct' : 100
 }
 
+
 def read_scd(filename):
-    fd = open(filename, 'r')
+    return read_adf11(filename, class_='scd')
+
+
+def read_acd(filename):
+    return read_adf11(filename, class_='acd')
+
+
+def read_adf11(name, class_):
+    fd = open(name, 'r')
 
     fortran_filename = 'fort.%d' % fd.fileno()
-    os.symlink(filename, fortran_filename)
+    os.symlink(name, fortran_filename)
 
-    iclass = 2 # class number for scd files
+    try:
+        iclass = adf11_classes[class_]
+    except IndexError:
+        raise NotImplementedError('unknown adf11 class: %s' % class_)
+
     ret =  _xxdata_11.xxdata_11(fd.fileno(), iclass, **parameters)
     os.unlink(fortran_filename)
-    return ret
+    return convert_to_dictionary(ret)
 
 
 def convert_to_dictionary(out):
