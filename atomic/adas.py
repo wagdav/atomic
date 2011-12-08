a913d4d6c06f94eb5e2e3f0b8a091913b6d7bb61
David Wagner <david.wagner@epfl.ch>
2011-12-08 14:41:07 +0100

    Restructure the interface to the ADAS web interface.

--- atomic/adas.py
+++ atomic/adas.py
@@ -1,25 +1,68 @@
 import os
 import urllib
 
+open_adas = 'http://open.adas.ac.uk/'
 
 class OpenADAS(object):
-    search_url = ('http://open.adas.ac.uk/adf11.php?'
-            'element=%(element)s&'
-            'year=%(year)d&'
-            'metastable=%(metastable)s&'
-            'searching=1')
-    fetch_url = r'http://open.adas.ac.uk/download.php?id=%d'
+    def search_adf11(self, element, year='', metastable='unresolved'):
+        p = [('element', element), ('year', year), ('metastable', metastable),
+                ('searching', 1)]
+        s = AdasSearch('adf11')
+        return s.search(p)
 
-    def __init__(self):
+    def search_adf15(self, element, charge=''):
+        p = [('element', element), ('charge', charge), ('resolveby', 'file'),
+                ('searching', 1)]
+        s = AdasSearch('adf15')
+        return s.search(p)
+
+    def fetch(self, id_filename, dst_directory=None):
+        if dst_directory == None:
+            dst_directory = os.curdir
+        self.dst_directory = dst_directory
+
+        dst_filename = os.path.join(self.dst_directory, filename)
+
+        url = self._construct_url()
+        tmpfile, msg = urllib.urlretrieve(url)
+        lines = open(tmpfile).readlines()
+
+        # Write all lines in the destination file but the first.  Files, incorrectly,
+        # contain an empty first line. This is a bug in the Open-ADAS database.
+        dst = open(dst_filename, 'w')
+        dst.writelines(lines[1:])
+
+        os.remove(tmpfile)
+
+    def _construct_url(self, id_filename):
+        """
+        >>> db = OpenADAS()
+        >>> db._construct_url((12345, 'foo.dat'))
+        'http://open.adas.ac.uk/download.php?id=12345'
+        """
+        id_, filename = id_filename
+        query = [('id', id_)]
+        return open_adas + 'download.php?' + urllib.urlencode(query)
+
+
+class AdasSearch(object):
+    def __init__(self, class_):
+        if class_ not in ['adf11', 'adf15']:
+            raise NotImplementedError('ADAS class %s is not supported.' %s)
+
+        self.url = open_adas + '%s.php?' % class_
+        self.class_ = class_
         self.data = 0
+        self.parameters = []
 
-    def search(self, element, year):
-        self._retrieve_search_page(element, year)
+    def search(self, parameters):
+        self.parameters = parameters
+        self._retrieve_search_page()
         return self._parse_data()
 
-    def _retrieve_search_page(self, element, year):
-        options = dict(element=element, year=year, metastable='unresolved')
-        res, msg = urllib.urlretrieve(self.search_url % options)
+    def _retrieve_search_page(self):
+        search_url =  self.url + urllib.urlencode(self.parameters)
+        res, msg = urllib.urlretrieve(search_url)
         self.data = open(res).read()
         os.remove(res)
 
@@ -31,11 +74,19 @@ class OpenADAS(object):
         if lines == []: return {}
         header = lines.pop(0)
 
-        db = {}
+        db = []
         for l in lines:
-            element, class_, comment, year, resolved, url, name = l
-            id_ = self._strip_url(url)
-            db[class_.lower()] = (id_, name)
+            if self.class_ == 'adf11':
+                element, class_, comment, year, resolved, url, name = l
+                id_ = self._strip_url(url)
+                class_ = class_.lower()
+                db.append((id_, name))
+            elif self.class_ == 'adf15':
+                element, ion, w_lo, w_hi, url, name = l
+                id_ = self._strip_url(url)
+                db.append((id_, name))
+            else:
+                raise NotImplementedError('this should never happen')
 
         return db
 
@@ -43,30 +94,32 @@ class OpenADAS(object):
         _, id_ = url.split('=')
         return int(id_)
 
-    def fetch(self, id_filename, dst_directory=None):
-        if dst_directory == None:
-            dst_directory = os.curdir
-        self.dst_directory = dst_directory
-
-        id_, filename = id_filename
-
-        url = self.fetch_url % id_
-
-        dst_filename = os.path.join(self.dst_directory, filename)
-
-        tmpfile, msg = urllib.urlretrieve(url)
-        lines = open(tmpfile).readlines()
-
-        # Write all lines in the destination file but the first.  Files, incorrectly,
-        # contain an empty first line. This is a bug in the Open-ADAS database.
-        dst = open(dst_filename, 'w')
-        dst.writelines(lines[1:])
-
-        os.remove(tmpfile)
 
 
 from HTMLParser import HTMLParser
 class SearchPageParser(HTMLParser):
+    """
+    Filling in a search form on http://open.adas.ac.uk generates a HTML document
+    with a table that has the following structure:
+
+    >>> html = '''
+    ... <table class='searchresults'>
+    ...     <tr>
+    ...     <td>Ne</td> <td><a href='filedetail.php?id=32147'>rc89_ne.dat</a></td>
+    ...     <tr>
+    ...     </tr>
+    ...     <td>C</td> <td><a href='filedetail.php?id=32154'>rc89_c.dat</a></td>
+    ...     </tr>
+    ... </table>'''
+
+    The SearchPageParser can parse this document looking for a table with a
+    class `searchresults`.
+    >>> parser = SearchPageParser()
+    >>> parser.feed(html)
+    >>> for l in parser.lines: print l
+    ['Ne', 'filedetail.php?id=32147', 'rc89_ne.dat']
+    ['C', 'filedetail.php?id=32154', 'rc89_c.dat']
+    """
     def reset(self):
         self.search_results = False
         self.line = []
@@ -100,20 +153,5 @@ class SearchPageParser(HTMLParser):
 
 
 if __name__ == '__main__':
-    from atomic_data import _full_path
-
-    dst_directory = _full_path('')
-
-    data = [('carbon', 96), ('argon', 89), ('neon', 96)]
-
-    adas = OpenADAS()
-    for element, year in data:
-        res = adas.search(element, year)
-
-        for key in ['acd', 'scd', 'plt', 'prc', 'prb']:
-            if key in res.keys():
-                adas.fetch(res[key], dst_directory)
-            else:
-                print 'Warning: %s file is missing in %s %d dataset.'\
-                        % (key, element, year)
-
+    import doctest
+    doctest.testmod()
