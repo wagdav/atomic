e60075dd00a272db6e30ac0df4aa203e984d0b44
David Wagner <david.wagner@epfl.ch>
2011-12-08 16:09:33 +0100

    Start working on CoefficientFactory.

--- atomic/pec.py
+++ atomic/pec.py
@@ -1,21 +1,44 @@
 import numpy as np
+from scipy.interpolate import RectBivariateSpline
+
 from adf15 import Adf15
 
 class Transition(object):
-    def __init__(self, element, nuclear_charge, charge, datablock):
+    def __init__(self, type_, element, nuclear_charge, charge, wavelength,
+            temperature, density, pec):
         self.element = element
         self.nuclear_charge = nuclear_charge
         self.charge = charge
 
-        self.wavelength = datablock['wavelength'] * 1e-10
-        self.type_ = datablock['type']
+        self.wavelength = wavelength
+        self.type_ = type_
 
-        self.electron_density = datablock['density'] * 1e6
-        self.electron_temperature = datablock['temperature']
-        self.photon_emissivity = datablock['pec'] * 1e-6
+        self.electron_density = density
+        self.electron_temperature = temperature
+        self.photon_emissivity = pec
+
+    def interpolate(self, temperature_grid, density_grid, log=True):
+        if log:
+            x = np.log10(self.electron_temperature) * 1e-6
+            y = np.log10(self.electron_density)
+            z = np.log10(self.photon_emissivity)
+        else:
+            x = self.temperature_grid
+            y = self.density_grid
+            z = self.photon_emissivity
+
+        sp = RectBivariateSpline(x, y, z)
+        pec = sp(temperature_grid, density_grid).diagonal()
+
+        return self._on_new_grids(temperature_grid, density_grid, pec)
+
+    def _on_new_grids(self, new_temperature, new_density, new_pec):
+        return self.__class__(self.type_, self.element, self.nuclear_charge,
+                self.charge, self.wavelength, new_temperature, new_density,
+                new_pec)
 
 
-class PhotonEmissivity(object):
+class TransitionPool(object):
     def __init__(self, transitions=None):
         if transitions == None: transitions = []
 
@@ -29,7 +52,14 @@ class PhotonEmissivity(object):
         datablocks = f['datablocks']
 
         for d in datablocks:
-            t = Transition(element, nuclear_charge, charge, d)
+            wavelength = d['wavelength'] * 1e-10
+            temperature = d['temperature']
+            density = d['density'] * 1e6
+            pec = d['pec'] * 1e-6
+            type_ = d['type']
+
+            t = Transition(type_, element, nuclear_charge, charge, wavelength,
+                    temperature, density, pec)
             self.transitions.append(t)
 
     def filter_type(self, *type_names):
@@ -61,6 +91,12 @@ class PhotonEmissivity(object):
 
         return power
 
+    def interpolate(self, temperature_grid, density_grid):
+        new_transitions = [t.interpolate(temperature_grid, density_grid) for t
+                in self.transitions]
+
+        return self.__class__(new_transitions)
+
     @property
     def wavelengths(self):
         return np.array([t.wavelength for t in self.transitions])
@@ -77,6 +113,9 @@ class PhotonEmissivity(object):
     def coeffs(self):
         return np.array([t.photon_emissivity for t in self.transitions])
 
+    def __iter__(self):
+        return self.transitions.__iter__()
+
 
 def wavelength_to_joule(lambda_):
     from scipy.constants import h, c
@@ -90,10 +129,54 @@ def P_bremsstrahlung(k, Te, ne):
     return 1.53e-38 * Te**0.5 * k**2
 
 
-if __name__ == '__main__':
-    pec = PhotonEmissivity()
-    pec.append_file('pec96#c_pju#c3.dat')
+def fetch_pec_data():
+    from atomic.adas import OpenAdas
+    db = OpenAdas()
+    res = db.search_adf15('carbon')
+    for r in res:
+        db.fetch(r, 'adas_data/pec')
 
+
+from collections import defaultdict
+class CoefficientFactory(object):
+    def __init__(self, nuclear_charge, transition_pool):
+        self.nuclear_charge = nuclear_charge
+        self.transition_pool = transition_pool
+        self.ionisation_stages = {}
+
+    def create(self, temperature_grid, density_grid):
+        self._sort_by_ionisation_stages()
+
+    def _sort_by_ionisation_stages(self):
+        d = defaultdict(TransitionPool)
+        for t in self.transition_pool:
+            if not self._conforming(t): continue
+
+            d[t.charge].transitions.append(t)
+
+        self.ionisation_stages.update(d)
+
+    def _conforming(self, t):
+        return t.nuclear_charge == self.nuclear_charge
+
+
+if __name__ == '__main__':
+    pec = TransitionPool()
+
+    import glob
+
+    for f in glob.glob('adas_data/pec/pec96#c_pju*.dat'):
+        pec.append_file(f)
+
+
+    density = np.logspace(18, 20, 10)
+    temperature = np.logspace(0, 3, 10)
+
+    fact = CoefficientFactory(6, pec.filter_type('ex'))
+    coeff = fact.create(temperature, density)
+
+
+if 0:
     power = pec.filter_type('rec', 'ex').sum_transitions()
     te = pec.transitions[0].electron_temperature
     ne = pec.transitions[0].electron_density
