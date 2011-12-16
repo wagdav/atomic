13be788b83f18af267c2719dc1d72c0029c1202c
David Wagner <david.wagner@epfl.ch>
2011-12-16 19:15:39 +0100

    Use different coefficients.

--- examples/filter_ensemble_average.py
+++ examples/filter_ensemble_average.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     from atomic.pec import TransitionPool
     ad = atomic.element('argon')
-    tp = TransitionPool.from_adf15('adas_data/pec/*ar*.dat')
+    tp = TransitionPool.from_adf15('adas_data/pec/transport_llu#ar*.dat')
     ad = tp.filter_energy(2e3, 20e3, 'eV').create_atomic_data(ad)
 
     rt = atomic.RateEquations(ad)
@@ -20,18 +20,14 @@ if __name__ == '__main__':
 
     taus = np.array([ 1e14, 1e15, 1e16, 1e17, 1e18])/density
 
-    plt.figure(1); plt.clf()
-    from filter_construction import plot_coeffs
-    plot_coeffs(ad, temperature, 5)
-    plt.ylim(1e-35, 1e-30)
-    plt.draw()
-
     plt.figure(2); plt.clf()
     time_dependent_power(y, taus)
+    plt.ylim(ymin=1e-35)
     plt.draw()
 
     plt.figure(3); plt.clf()
     time_dependent_power(y, taus, ensemble_average=True)
+    plt.ylim(ymin=1e-35)
     plt.draw()
 
 
